9b965a822fbaa7f8a284c0958cefbd0a93cb0cca
kahil-nayton <kahil.nayton@coinbase.com>
2022-04-29 20:09:13 -0400

    add configs

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['next/core-web-vitals', 'next', 'prettier'],
+  plugins: ['@cb', '@typescript-eslint'],
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: ['../*'],
+            message:
+              'Do not use relative parent imports "../*". Use absolute imports instead.',
+          },
+        ],
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports' },
+    ],
+  },
+}
