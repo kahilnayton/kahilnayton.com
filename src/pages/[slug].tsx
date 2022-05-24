028b803294fdc68d9d42556a5001de6428c40a4e
kahil nayton <kahilnayton@gmail.com>
2022-05-23 22:33:25 -0400

    add static paths

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -3,6 +3,7 @@ import SEO from '@/components/SEO'
 import Hero from '@/components/shared/ui/Hero'
 import { links } from '@/lib'
 import { getContentModel, GetContentModelParams } from '@/utils/contentful'
+import type { GetStaticPaths } from 'next'
 import ErrorPage from 'next/error'
 
 const About = ({ page }: { page: any }) => {
@@ -27,5 +28,12 @@ export const getServerSideProps = async (params: GetContentModelParams) => {
   })
   return {
     props: { page },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
   }
 }
