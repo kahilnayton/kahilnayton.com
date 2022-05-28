be34c8f35c4da133146ae06b48de7e9a448e3736
kahil nayton <kahilnayton@gmail.com>
2022-05-28 16:28:05 -0400

    update env names

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 export const CONTENTFUL_SPACE_CONFIGS = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID ?? '',
-  accessToken: process.env.CONTENTFUL_CONTENT_DELIVERY_API_KEY ?? '',
-  previewAccessToken: process.env.CONTENTFUL_CONTENT_PREVIEW_API_KEY ?? '',
-  host: process.env.CONTENTFUL_PUBLIC_CONTENTFUL_HOST_EAA ?? '',
-  previewHost: process.env.CONTENTFUL_PUBLIC_CONTENTFUL_PREVIEW_HOST ?? '',
-  environment: process.env.CONTENTFUL_ENV ?? '',
+  spaceId: process.env.NEXT_PUBLIC_SPACE_ID ?? '',
+  accessToken: process.env.NEXT_PUBLIC_CONTENT_DELIVERY_API_KEY ?? '',
+  previewAccessToken: process.env.NEXT_PUBLIC_CONTENT_PREVIEW_API_KEY ?? '',
+  host: process.env.NEXT_PUBLIC_CONTENTFUL_HOST ?? '',
+  previewHost: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_HOST ?? '',
+  environment: process.env.NEXT_PUBLIC_CONTENTFUL_ENV ?? '',
 }
 
 export const NEXT_CONFIGS = {
